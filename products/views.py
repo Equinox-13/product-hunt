4d3ebc7d25e31a514caed0276863ff614ade3586
Equinox-13 <quaid.johar33@gmail.com>
2020-03-28 20:12:49 +0530

    Added Detail page functionality

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render ,redirect
+from django.shortcuts import render ,redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Product
 from django.utils import timezone
@@ -14,14 +14,22 @@ def create(request):
             product.title = request.POST['title']
             product.body = request.POST['body']
             # TODO if url field does not work correctly add condition
+            if request.POST['url'].startswith('http://') or request.POST['url'].startswith('https://'):
+                product.url = request.POST['url']
+            else:
+                product.url = 'http://'+ request.POST['url']
             product.url = request.POST['url']
             product.icon = request.FILES['icon']
             product.image = request.FILES['image']
             product.pub_date = timezone.datetime.now()
             product.hunter = request.user
             product.save()
-            return redirect('home')
+            return redirect('/products/'+str(product.id))
         else:
             return render(request, 'products/create.html', {'error':'All fields are required.'})
 
     return render(request, 'products/create.html')
+
+def detail(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    return render(request, 'products/detail.html', {'product':product})
