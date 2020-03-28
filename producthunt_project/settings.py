851bf27caab0f985a6d776d19ebae8f5e1a52e51
Equinox-13 <quaid.johar33@gmail.com>
2020-03-28 17:55:58 +0530

    Added creating product functionality

--- producthunt_project/settings.py
+++ producthunt_project/settings.py
@@ -128,3 +128,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'producthunt_project/static/')
 ]
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- products/views.py
+++ products/views.py
@@ -1,4 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render ,redirect
+from django.contrib.auth.decorators import login_required
+from .models import Product
+from django.utils import timezone
 
 def home(request):
     return render(request, 'products/home.html')
+
+@login_required
+def create(request):
+    if request.method == 'POST':
+        if request.POST['title'] and request.POST['body'] and request.POST['url'] and request.FILES['icon'] and request.FILES['image']:
+            product = Product()
+            product.title = request.POST['title']
+            product.body = request.POST['body']
+            # TODO if url field does not work correctly add condition
+            product.url = request.POST['url']
+            product.icon = request.FILES['icon']
+            product.image = request.FILES['image']
+            product.pub_date = timezone.datetime.now()
+            product.hunter = request.user
+            product.save()
+            return redirect('home')
+        else:
+            return render(request, 'products/create.html', {'error':'All fields are required.'})
+
+    return render(request, 'products/create.html')
