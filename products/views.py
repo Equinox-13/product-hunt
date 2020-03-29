500c8ac0655ba5425e768eb8faf4b0665bbeeb0c
Equinox-13 <quaid.johar33@gmail.com>
2020-03-29 12:03:53 +0530

    Added homepage functionality

--- products/views.py
+++ products/views.py
@@ -4,7 +4,8 @@ from .models import Product
 from django.utils import timezone
 
 def home(request):
-    return render(request, 'products/home.html')
+    products = Product.objects
+    return render(request, 'products/home.html', {'products':products})
 
 @login_required
 def create(request):
@@ -33,3 +34,12 @@ def create(request):
 def detail(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
     return render(request, 'products/detail.html', {'product':product})
+
+@login_required
+def upvote(request, product_id):
+    if request.method == 'POST':
+        product = get_object_or_404(Product, pk=product_id)
+        product.votes_total += 1
+        product.save()
+        return render(request, 'products/detail.html', {'product':product})
+
